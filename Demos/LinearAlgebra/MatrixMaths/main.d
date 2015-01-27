a9192c1b0b9a01b11a7cf56018f49a523759a335
Rob Rau <rfr2@mac.com>
2015-01-27 10:42:43 -0500

    First commit of Linear Algebra library. There is really no code in it yet, but I have real life things I need to do now :(

---
+++ Demos/LinearAlgebra/MatrixMaths/main.d
@@ -0,0 +1,15 @@
+﻿module main;
+
+import std.stdio;
+
+import LinearAlgebra.Matrix;
+
+void main(string[] args)
+{
+	// Prints "Hello World" string in console
+	writeln("Hello World!");
+	
+	// Lets the user press <Return> before program returns
+	stdin.readln();
+}
+
---
+++ LinearAlgebra/Matrix.d
@@ -0,0 +1,20 @@
+﻿module LinearAlgebra.Matrix;
+
+class Matrix(size_t r, size_t c, T = real)
+{
+	this(in T[r*c] values...)
+	{
+		foreach(size_t i, ref value; values)
+		{
+			mData[i] = value;
+		}
+	}
+
+
+private:
+	size_t mRows = r;
+	size_t mCols = c;
+
+	T[r*c] mData;
+}
+
