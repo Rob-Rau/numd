53439c65e91055673976e5cd8675c417faf8e669
Rob Rau <rfr2@mac.com>
2015-11-08 12:43:49 -0500

    Added a row reducer algo. Matrix inverse also implimented

--- LinearAlgebra/Matrix.d
+++ LinearAlgebra/Matrix.d
@@ -1,7 +1,39 @@
 ﻿module LinearAlgebra.Matrix;
 
-import std.stdio;
 import std.conv;
+import std.stdio;
+import std.complex;
+import std.exception;
+
+struct Mcomplex(T)
+{
+	alias mData this;
+	Complex!T mData;
+	alias Mcomplex!T ThisType;
+
+	void opAssign(T rhs)
+	{
+		mData.re = rhs;
+	}
+
+	void opAssign(Complex!T rhs)
+	{
+		mData = rhs;
+	}
+
+	ThisType[] opBinary(string op)(ThisType[] rhs)
+	{
+		static if((op == "*") || (op == "-") || (op == "+") || (op == "/"))
+		{
+			ThisType[] res;
+			foreach(int i, ref ele; rhs)
+				mixin("res[i].mData = mData"~op~"ele;");
+				//res[i] = mData*ele;
+
+			return res;
+		}
+	}
+}
 
 class Matrix(size_t r, size_t c, T = real)
 {
@@ -14,7 +46,14 @@ class Matrix(size_t r, size_t c, T = real)
 
 	this()
 	{
-		mData[] = 0;
+		static if(is(T : Mcomplex!real))
+		{
+			mData[] = Mcomplex!real(Complex!real(0));
+		}
+		else
+		{
+			mData[] = 0;
+		}
 	}
 
 	this(ThisType mat)
@@ -91,6 +130,184 @@ class Matrix(size_t r, size_t c, T = real)
 		return true;
 	}
 
+	ThisType rref()
+	{
+		auto rref = new ThisType(mData);
+
+		int currRow = 0, currCol = 0;
+		while((currRow < r) && (currCol < c))
+		{
+			if(rref.mData[currRow*c + currCol] != 0)
+			{
+				T[] row;
+				row = rref.mData[currRow*c..currRow*c+c];
+
+				// Normalize row so first entry is 1
+				if(rref.mData[currRow*c + currCol] != 1)
+				{
+					auto first = rref.mData[currRow*c + currCol];
+					row[] /= first;
+					debug writeln("Normalizing row " ~ to!string(currRow));
+					debug writeln(rref.ToString());
+				}
+				// Find all non-zero rows in this column and scale the current 
+				// row by the value in other row, then subtract current row from
+				// from other
+				for(int j = 0; j < r; j++)
+				{
+					if(j != currRow)
+					{
+						T[] nextRow;
+						nextRow = rref.mData[j*c..j*c+c];
+						if(rref.mData[j*c + currCol] != 0)
+						{
+							auto first = rref.mData[j*c + currCol];
+							nextRow[] -= (first*row[])[];
+							debug writeln("Subtracting row " ~ to!string(currRow) ~ " from " ~ to!string(j));
+							debug writeln(rref.ToString());
+						}
+					}
+				}
+				currRow++;
+				currCol++;
+			}
+			else if((currRow == r-1) && (currCol == c - 1))
+			{
+				currCol++;
+			}
+			// See if there is a row that has a non-zero entry further down
+			// if so swap it with current row
+			else
+			{
+				for(int j = (currRow+1); j < r; j++)
+				{
+					// Swap this row with current row
+					if(rref.mData[j*c + currCol] != 0)
+					{
+						debug writeln("swapping row " ~ to!string(currRow) ~ " with " ~ to!string(j));
+						T[c] swapRow;
+						swapRow[] = rref.mData[j*c..j*c+c];
+						rref.mData[j*c..j*c+c] = rref.mData[currRow*c..currRow*c+c];
+						rref.mData[currRow*c..currRow*c+c] = swapRow[];
+						debug writeln(rref.ToString());
+						break;
+					}
+					// All entries in this column are 0, move on
+					else if(j == r-1)
+					{
+						//currRow++;
+						currCol++;
+					}
+				}
+				//currCol++;
+			}
+		}
+		return rref;
+	}
+	// Things that only apply to squar matricies.
+	static if(c == r)
+	{
+		static ThisType Identity()
+		{
+			//assert(r == c, "Not a square matrix, no identity");
+			auto ident = new ThisType;
+			//ident.mData[] = 0;
+			for(int i = 0; i < r*c; i += r+1)
+			{
+				ident.mData[i] = 1;
+			}
+			return ident;
+		}
+
+		ThisType Inverse()
+		{
+			auto inv = Identity();
+			auto rref = new ThisType(mData);
+
+			int currRow = 0, currCol = 0;
+			while((currRow < r) && (currCol < c))
+			{
+				if(rref.mData[currRow*c + currCol] != 0)
+				{
+					T[] row, invRow;
+					row = rref.mData[currRow*c..currRow*c+c];
+					invRow = inv.mData[currRow*c..currRow*c+c];
+					// Normalize row so first entry is 1
+					if(rref.mData[currRow*c + currCol] != 1)
+					{
+						auto first = rref.mData[currRow*c + currCol];
+						row[] /= first;
+						invRow[] /= first;
+						debug writeln("Normalizing row " ~ to!string(currRow));
+						debug writeln(rref.ToString());
+					}
+					// Find all non-zero rows in this column and scale the current 
+					// row by the value in other row, then subtract current row from
+					// from other
+					for(int j = 0; j < r; j++)
+					{
+						if(j != currRow)
+						{
+							T[] nextRow, invNexRow;
+							nextRow = rref.mData[j*c..j*c+c];
+							invNexRow = inv.mData[j*c..j*c+c];
+
+							if(rref.mData[j*c + currCol] != 0)
+							{
+								auto first = rref.mData[j*c + currCol];
+								nextRow[] -= (first*row[])[];
+								invNexRow[] -= (first*invRow[])[];
+								debug writeln("Subtracting row " ~ to!string(currRow) ~ " from " ~ to!string(j));
+								debug writeln(rref.ToString());
+							}
+						}
+					}
+					currRow++;
+					currCol++;
+				}
+				else if((currRow == r-1) && (currCol == c - 1))
+				{
+					currCol++;
+				}
+				// See if there is a row that has a non-zero entry further down
+				// if so swap it with current row
+				else
+				{
+					for(int j = (currRow+1); j < r; j++)
+					{
+						// Swap this row with current row
+						if(rref.mData[j*c + currCol] != 0)
+						{
+							debug writeln("swapping row " ~ to!string(currRow) ~ " with " ~ to!string(j));
+							T[c] swapRow, invSwapRow;
+							swapRow[] = rref.mData[j*c..j*c+c];
+							invSwapRow[] = inv.mData[j*c..j*c+c];
+
+							rref.mData[j*c..j*c+c] = rref.mData[currRow*c..currRow*c+c];
+							inv.mData[j*c..j*c+c] = inv.mData[currRow*c..currRow*c+c];
+
+							rref.mData[currRow*c..currRow*c+c] = swapRow[];
+							inv.mData[currRow*c..currRow*c+c] = invSwapRow[];
+							debug writeln(rref.ToString());
+							break;
+						}
+						// All entries in this column are 0, move on
+						else if(j == r-1)
+						{
+							//currRow++;
+							currCol++;
+						}
+					}
+					//currCol++;
+				}
+			}
+
+			enforce(rref == ThisType.Identity(), "Matrix not invertible");
+
+			return inv;
+		}
+	}
+
 	string ToString()
 	{
 		string matStr;
@@ -116,18 +333,41 @@ package:
 	T[r*c] mData;
 }
 
+// opEquals
+unittest
+{
+	auto m1 = new Matrix!(3, 2)(25, 28,
+		57, 64,
+		89, 100);
+	
+	auto m2 = new Matrix!(3, 2)(25, 28,
+		57, 64,
+		89, 100);
+	
+	assert(m1 == m2, "Matrix equality test failed");
+}
+
+// Identity matrix generation test.
+unittest
+{
+	auto ident = Matrix!(3, 3).Identity();
+	auto testIdent = new Matrix!(3, 3)(1, 0, 0, 0, 1, 0, 0, 0, 1);
+
+	assert(ident == testIdent, "Matrix identity failed");
+}
+
 // op =
 unittest
 {
 	auto m1 = new Matrix!(2, 2)(1, 2,
 		3, 4);
-	
+
 	auto m2 = new Matrix!(2,2)(m1);
 	
 	assert(m1 == m2, "Matrix assignment test failed");
-	
+
+	// Force failure
 	m1.mData[0] = 3;
-	
 	assert(m1 != m2, "Matrix assignment test failed");
 }
 
@@ -139,7 +379,7 @@ unittest
 		5, 6);
 	auto m2 = new Matrix!(2, 2)(7, 8,
 		9, 10);
-	
+
 	auto m3 = m1 * m2;
 	
 	auto expected = new Matrix!(3, 2)(25, 28,
@@ -167,20 +407,6 @@ unittest
 		11, 13);
 	
 	assert(m3 == expected, "Matrix addition test failed");
-}
-
-// opEquals
-unittest
-{
-	auto m1 = new Matrix!(3, 2)(25, 28,
-		57, 64,
-		89, 100);
-	
-	auto m2 = new Matrix!(3, 2)(25, 28,
-		57, 64,
-		89, 100);
-	
-	assert(m1 == m2, "Matrix equality test failed");
 }
 
 // op scalar *
@@ -216,4 +442,86 @@ unittest
 		5, 6);
 	
 	assert(m2 == expected, "Matrix scalar division test failed");
+}
+
+unittest
+{
+
+	auto m1 = new Matrix!(2, 2)(2, 4, 6, 8);
+
+	auto m2 = m1.rref();
+
+	writeln(m2.ToString());
+
+	auto expected1 = Matrix!(2, 2).Identity();
+
+	assert(m2 == expected1, "Matrix rref test failed");
+
+	auto m3 = new Matrix!(3, 3)(2, 4, 6, 8, 10, 12, 14, 16, 18);
+
+	//writeln(m3.ToString());
+
+	auto m4 = m3.rref();
+
+	auto expected2 = new Matrix!(3, 3)(1, 0, -1, 0, 1, 2, 0, 0, 0);
+
+	writeln(m4.ToString());
+
+	assert(m4 == expected2, "Matrix rref test failed");
+
+	auto m5 = new Matrix!(3, 3)(0, 0, 2, 0, 5, 1, 6, 3, 1);
+	auto m6 = m5.rref();
+	auto expected3 = Matrix!(3, 3).Identity();
+	writeln(m6.ToString());
+	assert(m6 == expected3, "Matrix rref test failed");
+
+	/*
+	 0     4     5
+     0     0     7
+     0     3     1
+	*/
+	auto m7 = new Matrix!(3, 3)(0, 4, 5, 0, 0, 7, 0, 3, 1);
+	auto m8 = m7.rref();
+	auto expected4 = new Matrix!(3, 3)(0, 1, 0, 0, 0, 1, 0, 0, 0);
+	writeln(m8.ToString());
+	assert(m8 == expected4, "Matrix rref test failed");
+
+
+	auto m9 = new Matrix!(3, 6)(0, 0, 2, 1, 0, 0, 0, 5, 1, 0, 1, 0, 6, 3, 1, 0, 0, 1);
+	auto m10 = m9.rref();
+	writeln(m10.ToString());
+	/*
+	-0.0333   -0.1000    0.1667
+	-0.1000    0.2000        0
+	0.5000         0         0
+	*/
+	auto expected5 = new Matrix!(3, 6, real)(1, 0, 0, -1.0/30.0, -1.0/10.0, 1.0/6.0, 0, 1, 0, -1.0/10.0, 1.0/5.0, 0, 0, 0, 1.0, 1.0/2.0, 0, 0);
+	//writeln(expected5.ToString());
+	assert(m10 == expected5, "Matrix rref test failed");
+}
+
+unittest
+{
+	auto m1 = new Matrix!(3, 3, Mcomplex!real)();
+}
+
+unittest
+{
+	auto m1 = new Matrix!(3, 3)(0, 0, 2, 0, 5, 1, 6, 3, 1);
+	auto m2 = m1.Inverse();
+	auto expected = new Matrix!(3, 3)(-1.0/30.0, -1.0/10.0, 1.0/6.0, -1.0/10.0, 1.0/5.0, 0, 1.0/2.0, 0, 0);
+	writeln(m2.ToString());
+	assert(m2 == expected, "Matrix inverse test failed");
+
+	auto m3 = new Matrix!(3, 3)(0, 4, 5, 0, 0, 7, 0, 3, 1);
+	Matrix!(3,3) m4;
+	try
+	{
+		m4 = m3.Inverse();
+		assert(false, "Matrix inverse test failed");
+	}
+	catch(Exception e)
+	{
+		writeln(e.msg);
+	}
 }
