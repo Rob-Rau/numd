048076abc23658dacf6b3f887c52c4a92a4d6d53
Rob Rau <rfr2@mac.com>
2016-02-24 09:55:15 -0500

    added meshgrid to utilities

--- numd/utility.d
+++ numd/utility.d
@@ -1,5 +1,26 @@
 module numd.utility;
 
+struct Meshgrid
+{
+	real[][] X;
+	real[][] Y;
+}
+
+Meshgrid meshgrid(real[] x, real[] y)
+{
+	Meshgrid mesh = {X: new real[][](y.length, x.length), Y: new real[][](y.length, x.length)};
+
+	for(int i = 0; i < y.length; i++)
+		for(int j = 0; j < x.length; j++)
+			mesh.X[i][j] = x[j];
+
+	for(int i = 0; i < y.length; i++)
+		for(int j = 0; j < x.length; j++)
+			mesh.Y[i][j] = y[i];
+
+	return mesh;
+}
+
 real[] linspace(real start, real end, int points)
 {
 	real h = (end-start)/(points-1);
